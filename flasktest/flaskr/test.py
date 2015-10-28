208b62e05321bbf76884a8b3273b7036b804d657
Andrew Hughes <andrew@amhughes.com>
2015-10-28 00:49:28 -0500

    More testing

---
+++ flasktest/flaskr/test.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request, session, g, redirect, url_for, \
+                    abort, render_template, flash
+
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+#@app.before_request
+#def before_request():
+
+#@app.teardown_request(exception)
+#def teardown_request(exception):
+
+@app.route('/')
+def test1():
+    return render_template('test1')
