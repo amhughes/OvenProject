a22138de81a07aa4de9d8efa544a447605850227
Andrew Hughes <andrew@amhughes.com>
2015-10-31 02:20:40 -0500

    Bug fixes, allowed control of light from webpage

--- PIDWeb.py
+++ PIDWeb.py
@@ -3,8 +3,6 @@ from flask import Flask, request, session, g, redirect, url_for, \
 import math
 import RPi.GPIO as GPIO
 
-
-DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -14,30 +12,20 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-@app.before_request
-def before_request():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    relay = GPIO.PWM(18, 0.5)
-    relay.start(0)
-
-@app.teardown_request(exception)
-def teardown_request(exception):
-    relay.stop()
-    GPIO.cleanup()
-    print('Done!')
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+relay = GPIO.PWM(18, 0.5)
+relay.start(0)
+OutV = 0
 
 @app.route('/')
 def show_entriest():
-    return render_template('show_entriest.html', Out=Out)
+    return render_template('show_entriest.html', Out=OutV)
 
 @app.route('/set', methods=['POST'])
 def set():
-    Out = request.form['Out']
-    relay.ChangeDutyCycle(Out)
+    OutV = float(request.form['out'])
+    relay.ChangeDutyCycle(OutV)
     flash('Set Updated')
     return redirect(url_for('show_entriest'))
 
@@ -56,22 +44,25 @@ def set():
 #    flash('Temperature Profile Updated')
 #    return redirect(url_for('show_entries'))
 
-#@app.route('/login', methods=['GET','POST'])
-#def login():
-#    error = None
-#    if request.method == 'POST':
-#        if request.form['username'] != app.config['USERNAME']:
-#            error = 'Invalid Username'
-#        elif request.form['password'] != app.config['PASSWORD']:
-#            error = 'Invalid Password'
-#        else:
-#            session['logged_in'] = True
-#            flash('You were logged in')
-#            return redirect(url_for('show_entries'))
-#    return render_template('login.html', error=error)
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid Username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
-#@app.route('/logout')
-#def logout():
-#    session.pop('logged_in', None)
-#    flash('You were logged out')
-#    return redirect(url_for('show_entries'))
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
