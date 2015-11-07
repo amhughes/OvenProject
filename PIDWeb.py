22271c4481f2e54402fd7fd01b588defde6433fa
Andrew Hughes <andrew@amhughes.com>
2015-11-07 10:02:56 -0600

    Fixed error

--- PIDWeb.py
+++ PIDWeb.py
@@ -34,5 +34,25 @@ def set():
     flash('Set Updated')
     return redirect(url_for('show_entriest'))
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid Username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('main'))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
