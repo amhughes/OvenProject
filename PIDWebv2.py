9efdc54d499d77d64507ae23a01ef9dd874ee300
Andrew Hughes <andrew@amhughes.com>
2016-03-18 01:31:46 -0500

    Forgot something on test

--- PIDWebv2.py
+++ PIDWebv2.py
@@ -155,7 +155,7 @@ def main():
     elif dat.status == 2:
         return render_template('ready.html', CurrT=CSP)
     elif dat.status == 3:
-        return render_template('run.html', CurrT=dat.T)
+        return render_template('run.html', CurrT=CurrentT)
     else:
         return render_template('postrun.html')
 
