6d5d42b4f8cf86d34b15ce63c47877bd5c5287dc
Andrew Hughes <andrew@amhughes.com>
2016-03-18 02:06:10 -0500

    Bugfixes

--- PIDWebv2.py
+++ PIDWebv2.py
@@ -92,9 +92,9 @@ class PIDloop(threading.Thread):
                 elif output < outMin:
                     output = outMin
                 relay.ChangeDutyCycle(output)
-                c.cursor_pos = (1, 0)
+                c.cursor_pos = (1, 1)
                 c.write_string('T:' + str(currentTemp) + ' SP:' + str(setPoint))
-                c.cursor_pos = (2, 0)
+                c.cursor_pos = (2, 1)
                 c.write_string('Out:' + str(output))
                 Told = currentTemp
 
