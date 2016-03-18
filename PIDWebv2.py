a51ccd748ce27d1b795984755a68a0ed76626694
Andrew Hughes <andrew@amhughes.com>
2016-03-18 01:53:57 -0500

    Switching from a class to global variables

--- PIDWebv2.py
+++ PIDWebv2.py
@@ -37,22 +37,20 @@ class datastore:
         self.kp = float(kpti)
         self.ki = float(kiti)
         self.kd = float(kdti)
-        self.sp = 100
-        self.Out = 0
-        self.T = 0
         self.timel = []
         self.spl = []
         self.Outl = []
         self.TL = []
-        self.kill = True
-        self.status = 0
     def record(self):
         self.TL.append(self.T)
         self.Outl.append(self.Out)
-    def setT(newT):
-        self.T = newT
 
-CurrentT = 50
+currentTemp = 0
+setPoint = 100
+output = 0
+status = 0
+killStatus = True
+
 #Status:
 #0 = Off
 #1 = Preheat: No Program
@@ -67,60 +65,56 @@ class PIDloop(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
-        global CurrentT
-        CurrentT = 200
-        dat.kill = False
-        timold = perf_counter()
+        global currentTemp, output, killStatus
+        killStatus = False
+        timOld = perf_counter()
         Told = thermocouple.get()
         outMin = 0
         outMax = 0
-        interr = 0
+        intErr = 0
         while not(dat.kill):
             tim = perf_counter()
-            if (tim-timold)>1:
-                timold = tim
-                dat.setT(thermocouple.get())
-                CurrentT = thermocouple.get()
+            if (tim-timOld)>1:
+                timOld = tim
+                currentTemp = thermocouple.get()
                 Trj = thermocouple.get_rj()
-                if dat.T < (Trj-10): break
-                err = dat.sp - dat.T
-                interr += dat.ki*err
-                if interr > outMax:
-                    interr = outMax
-                elif interr < outMin:
-                    interr = outMin
-                din = dat.T - Told
-                dat.Out = dat.kp*err + interr - dat.kd*din
-                if dat.Out > outMax:
-                    dat.Out = outMax
-                elif dat.Out < outMin:
-                    dat.Out = outMin
-                print(dat.T)
-                print(dat.sp)
-                print(dat.Out)
-                relay.ChangeDutyCycle(dat.Out)
+                if currentTemp < (Trj-10): break
+                err = setPoint - currentTemp
+                intErr += dat.ki*err
+                if intErr > outMax:
+                    intErr = outMax
+                elif intErr < outMin:
+                    intErr = outMin
+                din = currentTemp - Told
+                output = dat.kp*err + intErr - dat.kd*din
+                if output > outMax:
+                    output = outMax
+                elif output < outMin:
+                    output = outMin
+                relay.ChangeDutyCycle(output)
                 c.cursor_pos = (1, 0)
-                c.write_string('T:' + str(dat.T) + ' SP:' + str(dat.sp))
+                c.write_string('T:' + str(currentTemp) + ' SP:' + str(setPoint))
                 c.cursor_pos = (2, 0)
-                c.write_string('Out:' + str(dat.Out))
-                Told = dat.T
+                c.write_string('Out:' + str(output))
+                Told = currentTemp
 
 class RampLoop(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
+        global status, killStatus
         tmin = 0
         timoldr = perf_counter()
         dat.record()
-        while not(dat.kill):
+        while not(killStatus):
             timr = perf_counter()
             if (timr-timoldr)>60:
                 timoldr = timr
                 tmin += 1
                 dat.record()
                 if len(dat.TL) == len(dat.spl):
-                    dat.kill = True
-                    dat.status = 4
+                    killStatus = True
+                    status = 4
                     break
                 dat.sp = dat.spl[tmin]
 
@@ -136,8 +130,7 @@ thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units)
 c = CharLCD(0x27, numbering_mode=GPIO.BCM, rows=2, cols=16)
 PIDloopT = PIDloop()
 RampLoopT = RampLoop()
-dat.sp = 100
-CSP = 100
+setPoint = 100
 
 def exitClean():
     relay.stop()
@@ -148,37 +141,38 @@ atexit.register(exitClean)
 
 @app.route('/')
 def main():
-    if dat.status == 0:
+    if status == 0:
         return render_template('prerun.html')
-    elif dat.status == 1:
+    elif status == 1:
         return render_template('program.html')
-    elif dat.status == 2:
-        return render_template('ready.html', CurrT=CSP)
-    elif dat.status == 3:
-        return render_template('run.html', CurrT=CurrentT)
+    elif status == 2:
+        return render_template('ready.html', CurrT=currentTemp)
+    elif status == 3:
+        return render_template('run.html', CurrT=currentTemp)
     else:
         return render_template('postrun.html')
 
 @app.route('/preheat', methods=['POST'])
 def preheat():
-    global CSP
+    global status
     PIDloopT.start()
-    CSP = 150
     flash('Preheat Enabled')
-    dat.status = 1
+    status = 1
     return redirect(url_for('main'))
 
 @app.route('/startrun', methods=['POST'])
 def startrun():
+    global status
     flash('Run Started')
-    dat.status = 3
+    status = 3
     return redirect(url_for('main'))
 
 @app.route('/kill', methods=['POST'])
 def kill():
-    dat.kill = True
+    global status, killStatus
+    killStatus = True
     flash('Run Stopped')
-    dat.status = 0
+    status = 0
     return redirect(url_for('main'))
 
 @app.route('/tune', methods=['POST'])
@@ -196,6 +190,7 @@ def tune():
 
 @app.route('/profile', methods=['POST'])
 def profile():
+    global status
     RunName = request.form['Name']
     HoldT = float(request.form['HoldT'])
     HoldTim = int(request.form['HoldTim'])
@@ -232,7 +227,7 @@ def profile():
         dat.timel.append(i)
         opf.write(str(dat.timel[i]) + ' ' + str(dat.spl[i]) + '\n')
     opf.close()
-    dat.status = 2
+    status = 2
     flash('Temperature Profile Updated')
     return redirect(url_for('main'))
 
