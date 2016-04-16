a58e403af0e658868926bd8ee31b3bb5b7590077
Andrew Hughes <andrew@amhughes.com>
2016-04-16 10:58:14 -0500

    Final CSV read test before integration

--- test/csvtest.py
+++ test/csvtest.py
@@ -1,6 +1,13 @@
 import csv
 
+intl = []
+floatl = []
+
 with open('test.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
     for row in spamreader:
-        print(row)
+        intl.append(int(row[0]))
+        floatl.append(row[1])
+
+print(intl)
+print(floatl)
