3b6d1aac59b1fc8d96895f2f964d147fc5b9982d
Andrew Hughes <andrew@amhughes.com>
2016-04-18 23:50:02 -0500

    Removed unnecessary files for release

--- test/lcdtest.py
+++
@@ -1,9 +0,0 @@
-import time
-from RPLCD import CharLCD
-from RPLCD import Alignment, CursorMode, ShiftMode
-from RPLCD import cursor, cleared
-c = CharLCD(0x27, rows=2, cols=16)
-c.write_string('Raspberry Pi HD44780')
-c.cursor_pos = (2, 0)
-c.write_string('http://github.com/\n\rdbrgn/RPLCD')
-c.close()
